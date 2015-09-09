46ebe78224495d111ba99a5612c55ec204931640
Howon Lee <howon@howonlee.com>
2015-09-08 17:52:18 -0700

    first actual commit:

---
+++ staircase.py
@@ -0,0 +1,5 @@
+# 1. show the staircases from a github repo
+
+# 2. ansatz: say that the cantor functions are made from a hierarchy of ifs-like functions _akin_ to the cantor function itself, but see if there are any _patterns_ to the ifs
+
+# 3. go look at per bak's stuff a lot
