0ebfa1e650c0bbf7d727efb1f2e904a793e1fe99
Howon Lee <howon@howonlee.com>
2015-10-16 11:23:07 -0700

    think I understand anim better?

--- parse.py
+++ parse.py
@@ -1,13 +1,93 @@
 import arrow
 import matplotlib.pyplot as plt
 import numpy as np
+import itertools as it
+import pylab
+import matplotlib
+from matplotlib import animation
+from mpl_toolkits.mplot3d import Axes3D
+
+def plot_sec_hist(seconds):
+    YBASE = 2; XBASE = 2
+    MIN_VAL = 1; MAX_VAL = np.ceil(max(seconds))
+    start_power = np.floor(np.log(MIN_VAL) / np.log(XBASE))
+    end_power = np.ceil(np.log(MAX_VAL) / np.log(XBASE))
+    num_bins = (end_power - start_power) + 1
+    bins = np.logspace(start_power, end_power, num_bins, base=XBASE)
+    hist = np.histogram(seconds, bins=bins)
+    plt.loglog(hist[1][:-1], hist[0], 'x-', basey=YBASE, basex=XBASE)
+    plt.show()
+
+def plot_seconds(seconds):
+    plt.plot(seconds)
+    plt.show()
+
+def mi(x,y, bins=11):
+    """Given two arrays x and y of equal length, return their mutual information in bits
+    """
+    Hxy, xe, ye = pylab.histogram2d(x,y,bins=bins)
+    Hx = Hxy.sum(axis=1)
+    Hy = Hxy.sum(axis=0)
+    Pxy = Hxy/float(x.size)
+    Px = Hx/float(x.size)
+    Py = Hy/float(x.size)
+    pxy = Pxy.ravel()
+    px = Px.repeat(Py.size)
+    py = pylab.tile(Py, Px.size)
+    idx = pylab.find((pxy > 0) & (px > 0) & (py > 0))
+    return (pxy[idx]*pylab.log2(pxy[idx]/(px[idx]*py[idx]))).sum()
+
+def takens_embedding(data, tau_max=100, mode="2d"):
+    mis = []
+    for tau in range(1, tau_max):
+        unlagged = data[:-tau]
+        lagged = np.roll(data, -tau)[:-tau]
+        mis.append(mi(unlagged, lagged))
+
+        if len(mis) > 1 and mis[-2] < mis[-1]: # return first local minima
+            tau -= 1
+            print tau, mis
+            break
+    data_lag0 = data[:-2].flatten()
+    data_lag1 = np.roll(data, -tau)[:-2].flatten()
+    data_lag2 = np.roll(data, -2 * tau)[:-2].flatten()
+    if mode =="3d":
+        # 3d plot
+        figure = pylab.figure()
+        axes = Axes3D(figure)
+        axes.plot3D(data_lag0, data_lag1, data_lag2, linestyle='-', lw=5, alpha=0.4)
+        figure.add_axes(axes)
+        pylab.show()
+    elif mode == "2d":
+        plt.plot(data_lag0, data_lag1)
+        plt.show()
+    elif mode == "anim":
+        fig = plt.figure()
+        ax = plt.axes(xlim=(0, 90000), ylim=(0, 90000))
+        line, = ax.plot([], [], lw=2)
+        def init():
+            line.set_data([], [])
+            return line,
+        def animate(i):
+            line.set_data(data_lag0[:i], data_lag1[:i])
+            return line,
+        anim = animation.FuncAnimation(fig, animate, init_func=init, frames=1000, interval=20, blit=True)
+        anim.save("anim.mp4", fps=30)
+        plt.show()
 
 if __name__ == "__main__":
     seconds = []
-    with open("/home/curuinor/data/linux_sched") as sched_file:
+    #with open("/home/curuinor/data/linux_sched") as sched_file:
+    with open("sched") as sched_file:
         sched_lines = sched_file.readlines()
+        #sched_lines = sched_lines[:4000]
+        print "num lines: ", len(sched_lines)
         curr_date = arrow.get(sched_lines[0].strip()[4:], "MMM D HH:mm:ss YYYY Z")
+        i = 0
         for line in sched_lines[1:]:
+            i += 1
+            if i % 10000 == 0:
+                print i
             next_date = arrow.get(line.strip()[4:], "MMM D HH:mm:ss YYYY Z")
             diff = curr_date - next_date
             if diff.days != 0:
@@ -17,5 +97,8 @@ if __name__ == "__main__":
             curr_date = next_date
             #"Fri Oct 16 00:48:51 2015 -0000"
             #'Thu Oct 15 17:41:19 2015 -0700'
-    plt.hist(seconds, log=True)
-    plt.show()
+    print "finished reading"
+    seconds_avg = np.mean(seconds)
+    #plt.plot(np.abs(np.diff(seconds)))
+    #plt.show()
+    takens_embedding(np.array(seconds), mode="anim")
