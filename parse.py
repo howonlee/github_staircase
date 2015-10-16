6104aa3ff0715a57220c6b29210750836a7e0b15
Howon Lee <howon@howonlee.com>
2015-10-15 19:26:53 -0700

    parsing done

--- parse.py
+++ parse.py
@@ -1,10 +1,17 @@
 import arrow
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    seconds = []
     with open("sched") as sched_file:
-        for line in sched_file:
-            print line.strip()
-            print arrow.get(line.strip()[4:], "MMM D HH:mm:ss YYYY Z")
-
+        sched_lines = sched_file.readlines()
+        curr_date = arrow.get(sched_lines[0].strip()[4:], "MMM D HH:mm:ss YYYY Z")
+        for line in sched_lines[1:]:
+            next_date = arrow.get(line.strip()[4:], "MMM D HH:mm:ss YYYY Z")
+            diff = curr_date - next_date
+            seconds.append(diff.seconds)
+            curr_date = next_date
             #"Fri Oct 16 00:48:51 2015 -0000"
             #'Thu Oct 15 17:41:19 2015 -0700'
+    plt.hist(seconds)
+    plt.show()
